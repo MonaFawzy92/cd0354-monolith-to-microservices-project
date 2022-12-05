875d58e445c677d9dd11c12b9607da52deb18785
MonaFawzy92 <58727333+MonaFawzy92@users.noreply.github.com>
2022-12-05 21:56:25 +0200

    update nginx

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=postgres.c9jlxz0frjpg.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.chzamqpyj313.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=udacity-microservices-s3
+export AWS_BUCKET=udacitys3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
